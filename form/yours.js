7e6b09d434a18dd94da812bb7b41ce5efd5e12f0
tjdgus3160 <58248468+tjdgus3160@users.noreply.github.com>
2021-01-26 11:11:03 +0900

    Update yours.js

---
+++ form/yours.js
@@ -0,0 +1,24 @@
+const toggleButton=document.querySelector('.dropdown-toggle');
+const menu=document.querySelector('.dropdown-menu');
+const options=document.querySelectorAll('.dropdown-option');
+const nextButton=document.querySelector('.next-button');
+
+toggleButton.addEventListener('click',function(){
+  menu.classList.toggle('show');
+})
+toggleButton.addEventListener('blur', function () {
+  menu.classList.remove('show');
+})
+
+// options.forEach(function (item) {
+//     item.addEventListener('click', selectOption)
+//   })
+
+menu.addEventListener('click',function(e){
+    const value = e.target.textContent.trim();
+    console.log(value);
+    toggleButton.textContent=value;
+    toggleButton.classList.add('selected');
+    nextButton.removeAttribute('disabled');
+    }
+)
